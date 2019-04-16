e6b7222beddd469ba7cdc46b495d8a82676c6194
lichang333 <lichang@oregonstate.edu>
2019-04-16 12:13:52 +0800

    keep moving

--- 01-React/01-Tooling/03-ES6-Classes/lib/01_counter.js
+++ 01-React/01-Tooling/03-ES6-Classes/lib/01_counter.js
@@ -1,10 +1,18 @@
 class Counter {
   constructor(text) {
     // TODO: build an internal Map of word => occurrences.
+    this.map = new Map();
+    if (text && text.length > 0) {
+      text.split(" ").forEach((term) => {
+        const lowerTerm = term.toLowerCase();
+        this.map.set(lowerTerm, (this.map.get(lowerTerm) || 0) + 1);
+      });
+    }
   }
 
   occurrences(word) {
     // TODO: return the number of occurrences
+    return this.map.get(word.toLowerCase()) || 0;
   }
 }
 
